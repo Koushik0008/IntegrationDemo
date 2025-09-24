7af2ae27863d7c7c66f5751d6a841a031c35f8e0
Koushik0008 <koushikbelige@gmail.com>
2025-09-24 12:27:49 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,20 +1,12 @@
-import java.util.Scanner;
-
 public class Factorial {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a non-negative integer: ");
-        int number = scanner.nextInt();
-        scanner.close();
-
-        if (number < 0) {
-            System.out.println("Factorial is not defined for negative numbers.");
-        } else {
-            long factorial = 1; // Use long to handle larger factorials
-            for (int i = 1; i <= number; i++) {
-                factorial *= i;
-            }
-            System.out.println("Factorial of " + number + " is: " + factorial);
+        if (args.length == 0) {
+            System.out.println("Usage: java Factorial <non-negative integer>");
+            return;
         }
+        int n = Integer.parseInt(args[0]);
+        int fact = 1;
+        for (int i = 1; i <= n; i++) fact *= i;
+        System.out.println("Factorial of " + n + " is " + fact);
     }
 }
